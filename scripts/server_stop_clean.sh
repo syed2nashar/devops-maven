36e8df03441df2f7d794ec2f014afa853251a83f
syed2nashar <31008300+syed2nashar@users.noreply.github.com>
2022-12-18 22:47:37 +0530

    Update server_stop_clean.sh

--- scripts/server_stop_clean.sh
+++ scripts/server_stop_clean.sh
@@ -2,5 +2,6 @@
 
 # Stop Tomcat first
 sudo systemctl stop tomcat
-sudo rm -rf /usr/share/tomcat/webapps/hello-world-maven.war
+#sudo rm -rf /usr/share/tomcat/webapps/hello-world-maven.war
+sudo rm -rf /opt/apache-tomcat-8.5.84/webapps/hello-world-maven.war
 
