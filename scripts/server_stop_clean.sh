ea68a329defc688a20d4985454e457c472e4ee8d
syed2nashar <31008300+syed2nashar@users.noreply.github.com>
2022-12-19 00:33:02 +0530

    Update server_stop_clean.sh

--- scripts/server_stop_clean.sh
+++ scripts/server_stop_clean.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo cd /opt/apache-tomcat-8.5.84/bin
 $BASE/shutdown.sh 2>&1 > /dev/null
 #sudo rm -rf /usr/share/tomcat/webapps/hello-world-maven.war
 sudo rm -rf /opt/apache-tomcat-8.5.84/webapps/hello-world-maven.war
